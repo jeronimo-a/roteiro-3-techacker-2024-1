9e06a9cc46fe18c46a02b672bab254641d20c922
Jerônimo Afrange <jeronimoaa@al.insper.edu.br>
2024-05-11 14:18:25 -0300

    background detecta e armazena os acessos a hosts de terceira parte

--- scripts/background.js
+++ scripts/background.js
@@ -1,44 +1,85 @@
-// ################################################
-// ############## CHAMADAS DE FUNÇÃO ##############
-// ################################################
-
-const VERSION = browser.runtime.getManifest().version;
-browser.runtime.onMessage.addListener(handleMessage);
-
 // ##############################################
 // ############## VARIÁVEIS GERAIS ##############
 // ##############################################
 
-const ICON_DEFAULT = "icons/magnifier.png";
-const gDisplayNotifications = true;
+const ICON_DEFAULT = "icons/magnifier.png";				// local do ícone
+const VERSION = browser.runtime.getManifest().version;	// versão da extensão
+const gDisplayNotifications = true;						// habilita ou desabilita notificações
 
-// ########################################
-// ############### MENSAGENS ##############
-// ########################################
+let thirdPartyRequests = {};	// dicionário que relaciona o host principal de cada aba a um conjunto com os hosts de terceira parte
 
+// ################################################
+// ############## CHAMADAS DE FUNÇÃO ##############
+// ################################################
+
+browser.runtime.onMessage.addListener(handleMessage);	// define que função chamar ao receber uma mensagem de algum outro pedaço do código
+browser.webRequest.onBeforeRequest.addListener(			// define que função chamar antes de fazer um novo request
+    handleWebRequest,		// handler de web requests
+    {urls: ["<all_urls>"]}	// para que URLs
+);
+
+// #######################################
+// ############### HANDLERS ##############
+// #######################################
+
+// handler que roda ao receber uma mensagem de algum outro lugar do código
 function handleMessage(request, sender, sendResponse) {
 	detectorNotification.display("Teste", 2000);
+}
+
+// handler que roda antes de cada request da página
+function handleWebRequest(details) {
+
+	// extrai os argumentos e valida a aba
+    const {url, tabId} = details;
+    if (tabId < 0) return;
+
+	// extrai o hostname da URL do request
+    const fpHostname = new URL(url).hostname;
+
+    // Adiciona o hostname ao conjunto de hostnames associados ao hostname da aba
+	// A arrow function tab => {} tem tab como um parâmetro
+    chrome.tabs.get(tabId, tab => {
+
+		// se não houver erro
+        if (!chrome.runtime.lastError) {
+
+			// pega o hostname principal da aba, a partir do argumento tab
+            const tabHostname = new URL(tab.url).hostname;
+			
+			// se o hostname direcionado for diferente do principal da aba, adicona ele ao conjunto de hosts de terceira parte da aba
+            if (getDomainName(fpHostname) !== getDomainName(tabHostname)) {
+
+				// se não existir o conjunto para o tal hostname da aba, cria um antes de adicionar
+                if (!thirdPartyRequests[tabId]) { thirdPartyRequests[tabId] = new Set(); }
+                thirdPartyRequests[tabId].add(fpHostname);
+            }
+        }
+    });
 }
 
 // ############################################
 // ############### NOTIFICAÇÕES ###############
 // ############################################
 
+// objeto de notificações
 const detectorNotification = {
-	_notificationIcon: browser.extension.getURL(ICON_DEFAULT),
-	_notificationId: "dmaster2000",
-  
+	
+	// método display que cria uma notificação com um texto "text" por "millis" milissegundos
 	display: (text, millis) => {
-  
+		
+		// se as notificações estiverem desabilitadas, finaliza
 		if (!gDisplayNotifications) return;
-	
-		browser.notifications.create(detectorNotification._notificationId, {
-			"type": "basic",
-			"iconUrl": detectorNotification._notificationIcon,
-			"title": "Detector Master 2000",
-			"message": text
+		
+		// cria a notificação com o ID "dmaster2000"
+		browser.notifications.create("dmaster2000", {
+			"type": "basic",									// tipo da notificação
+			"iconUrl": browser.extension.getURL(ICON_DEFAULT),	// ícone da notifiação
+			"title": "Detector Master 2000",					// título da notificação
+			"message": text										// mensagem da notificação
 		});
-	
+		
+		// remove a notificação depois de "millis" milissegundos
 		window.setTimeout(() => {
 			browser.notifications.clear(razorNotification._notificationId);
 		}, millis);
